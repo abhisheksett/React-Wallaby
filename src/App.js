7eb66886e890701d78e17f9287e759089ef61eb6
abhisheksett <aabhishek.sett@gmail.com>
2017-09-29 21:15:13 +0530

    axios added

--- src/App.js
+++ src/App.js
@@ -1,52 +1,80 @@
-import React, { Component } from 'react'
-import Status from './components/Status'
-import Header from './components/Header'
-import Paper from './components/Paper'
-import Form from './components/Form'
-import ListView from './components/ListView'
-import { deposit, reduce } from './API/api'
+import React, { Component } from "react";
+import axios from "axios";
+import Status from "./components/Status";
+import Header from "./components/Header";
+import Paper from "./components/Paper";
+import Form from "./components/Form";
+import ListView from "./components/ListView";
+import { deposit, reduce } from "./API/api";
 
 class App extends Component {
-
   state = {
     balance: 100000,
     transactions: []
   };
 
   updateBalance = (operation, updatedValue, modifiedDate, note) => {
-    
-    if (operation === 'add-deposit') {
-      this.setState(deposit(parseInt(updatedValue)));
+    let updatedBalance;
+    if (operation === "add-deposit") {
+      updatedBalance = deposit(parseInt(updatedValue));
     } else {
-      this.setState(reduce(parseInt(updatedValue)));
+      updatedBalance = reduce(parseInt(updatedValue));
     }
+    axios
+      .post("http://localhost:3001/transactions", {
+        operation,
+        amount: updatedValue,
+        modifiedDate,
+        note,
+        updatedBalance: updatedBalance.balance
+      })
+      .then(data => {
+        if (operation === "add-deposit") {
+          this.setState(updatedBalance);
+        } else {
+          this.setState(updatedBalance);
+        }
 
-    this.setState((previousState) => ({
-      transactions: [...this.state.transactions, 
-        {
-          operation,
-          amount: updatedValue,
-          modifiedDate,
-          note,
-          updatedBalance: previousState.balance
-        }]
-    }), () => {
-      console.log(this.state.transactions);
+        this.setState(
+          previousState => ({
+            transactions: [
+              ...this.state.transactions,
+              {
+                operation,
+                amount: updatedValue,
+                modifiedDate,
+                note,
+                updatedBalance: previousState.balance
+              }
+            ]
+          }),
+          () => {
+            console.log(this.state.transactions);
+          }
+        );
+      });
+  };
+
+  componentDidMount = () => {
+    axios.get("http://localhost:3001/transactions").then(data => {
+      this.setState({
+        transactions: [...this.state.transactions, ...data.data]
+      });
     });
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <Paper>
           <Header />
-          <Status balance={this.state.balance}/>
-          <Form updateBalance={this.updateBalance}/>
+          <Status balance={this.state.balance} />
+          <Form updateBalance={this.updateBalance} />
           <ListView data={this.state.transactions} />
         </Paper>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
